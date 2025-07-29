e03b67079071e9c6cdec1ac74e91f1bb1d960821
akarsh363 <122525163+akarsh363@users.noreply.github.com>
2025-07-29 06:33:58 +0530

    Ex02OddEven.cs

--- Ex02OddEven.cs
+++ Ex02OddEven.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Assignments
 {
     internal class Ex02OddEven
     {
         static void Main(string[] args)
-        {
+        {   
             Console.WriteLine("Enter how many numbers you want to give as input");
             int count = Convert.ToInt32(Console.ReadLine());
+                        // int.parse(console.RadLine());
 
             int[] numbers = new int[count];
 
-            for(int i=0; i<count; i++)
+            for (int i = 0; i < count; i++)
             {
-                Console.WriteLine($"Enter the Number : {i+1}");
+                Console.WriteLine($"Enter the Number : {i + 1}");
                 numbers[i] = Convert.ToInt32(Console.ReadLine());
+                             // int.parse(console.Readline());
             }
 
             showOddEven(numbers);
         }
         static void showOddEven(int[] numbers)
         {
-            Console.WriteLine("Even Number");
+            Console.WriteLine("Even Numbers");
             foreach (int i in numbers)
             {
-                if (i % 2 == 0)
+                if (i%2 == 0)
                     Console.WriteLine(i);
             }
 
-            Console.WriteLine("Odd Number");
+            Console.WriteLine("Odd Numbers");
             foreach (int i in numbers)
             {
-                if(i % 2 == 1)
+                  if (i%2 != 0)
+                    // if(i%2 == 1)
+               
                     Console.WriteLine(i);
             }
         }
