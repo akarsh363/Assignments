01028b4818eed37042d279e9288ee9c67758ef0c
akarsh363 <122525163+akarsh363@users.noreply.github.com>
2025-08-08 11:42:14 +0530

    Add files via upload

---
+++ E15Constructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string personName, int personAge)
+        {
+            name = personName;
+            age = personAge;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Name:{name}, Age : {age}");
+        }
+    }
+    internal class E15Constructor
+    {
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Akarsh", 23);
+            p1.ShowDetails();
+        }
+    }
+}
+
