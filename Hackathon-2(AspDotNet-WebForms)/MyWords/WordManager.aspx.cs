82b4e7df5384ada7be7b704175d31781613facc1
akarsh363 <122525163+akarsh363@users.noreply.github.com>
2025-08-26 11:45:33 +0530

    Add files via upload

---
+++ Hackathon-2(AspDotNet-WebForms)/MyWords/WordManager.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWebFormsApp
+{
+    public partial class WordManager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string word = txtSearch.Text.Trim();
+            lblWordToTranslate.Text = word;
+
+            if (WordStore.ContainsWord(word))
+            {
+                lblSearchResult.Text = $"Found: {word}";
+                txtTranslation.Text = WordStore.Words[word];
+            }
+            else
+            {
+                lblSearchResult.Text = $"'{word}' is not present. You can add it with a translation.";
+                txtTranslation.Text = "";
+            }
+        }
+
+        protected void btnAddTranslation_Click(object sender, EventArgs e)
+        {
+            string word = lblWordToTranslate.Text;
+            string translation = txtTranslation.Text.Trim();
+
+            if (!string.IsNullOrEmpty(word) && !string.IsNullOrEmpty(translation))
+            {
+                WordStore.AddOrUpdate(word, translation);
+                BindGrid();
+                lblSearchResult.Text = $"'{word}' added/updated successfully.";
+            }
+
+        }
+        private void BindGrid()
+        {
+            gvWords.DataSource = WordStore.GetAllWords();
+            gvWords.DataBind();
+        }
+
+    }
+}
---
+++ Hackathon-2(AspDotNet-WebForms)/MyWords/WordStore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SampleWebFormsApp
+{
+    internal class WordStore
+    {
+        public static Dictionary<string, string> Words = new Dictionary<string, string>
+    {
+        { "sequel", "subsequent event" },
+        { "Adulation", "Excessive flattery" },
+        { "Fun", "enjoy" }
+    };
+
+        public static bool ContainsWord(string word)
+        {
+            return Words.ContainsKey(word);
+        }
+
+        public static void AddOrUpdate(string word, string translation)
+        {
+            Words[word] = translation;
+        }
+
+        public static Dictionary<string, string> GetAllWords()
+        {
+            return Words;
+        }
+    }
+}
